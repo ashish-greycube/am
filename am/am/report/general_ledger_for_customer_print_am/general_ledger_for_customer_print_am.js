91ef26c6b7339ce2422ec3f2e644e33b2f487627
Sakshi-Greycube <sakshi@greycube.in>
2025-08-22 17:57:34 +0530

    feat: New Report: General Ledger For Customer Print

---
+++ am/am/report/general_ledger_for_customer_print_am/general_ledger_for_customer_print_am.js
@@ -0,0 +1,261 @@
+// Copyright (c) 2025, GreyCube Technologies and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["General Ledger For Customer Print AM"] = {
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
+		{
+			fieldname: "finance_book",
+			label: __("Finance Book"),
+			fieldtype: "Link",
+			options: "Finance Book",
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1,
+			width: "60px",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+			width: "60px",
+		},
+		{
+			fieldname: "account",
+			label: __("Account"),
+			fieldtype: "MultiSelectList",
+			options: "Account",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Account", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+		},
+		{
+			fieldname: "voucher_no",
+			label: __("Voucher No"),
+			fieldtype: "Data",
+			on_change: function () {
+				frappe.query_report.set_filter_value("categorize_by", "Categorize by Voucher (Consolidated)");
+			},
+		},
+		{
+			fieldname: "against_voucher_no",
+			label: __("Against Voucher No"),
+			fieldtype: "Data",
+		},
+		{
+			fieldtype: "Break",
+		},
+		{
+			fieldname: "party_type",
+			label: __("Party Type"),
+			fieldtype: "Autocomplete",
+			options: Object.keys(frappe.boot.party_account_types),
+			on_change: function () {
+				frappe.query_report.set_filter_value("party", []);
+				get_customer_details()
+			},
+		},
+		{
+			fieldname: "party",
+			label: __("Party"),
+			fieldtype: "MultiSelectList",
+			options: "party_type",
+			get_data: function (txt) {
+				if (!frappe.query_report.filters) return;
+
+				let party_type = frappe.query_report.get_filter_value("party_type");
+				if (!party_type) return;
+
+				return frappe.db.get_link_options(party_type, txt);
+			},
+			on_change: function () {
+				var party_type = frappe.query_report.get_filter_value("party_type");
+				var parties = frappe.query_report.get_filter_value("party");
+
+				if (!party_type || parties.length === 0 || parties.length > 1) {
+					frappe.query_report.set_filter_value("party_name", "");
+					frappe.query_report.set_filter_value("tax_id", "");
+					return;
+				} else {
+					var party = parties[0];
+					var fieldname = erpnext.utils.get_party_name(party_type) || "name";
+					frappe.db.get_value(party_type, party, fieldname, function (value) {
+						frappe.query_report.set_filter_value("party_name", value[fieldname]);
+					});
+
+					if (party_type === "Customer" || party_type === "Supplier") {
+						frappe.db.get_value(party_type, party, "tax_id", function (value) {
+							frappe.query_report.set_filter_value("tax_id", value["tax_id"]);
+						});
+					}
+				}
+
+				get_customer_details()
+			},
+		},
+		{
+			fieldname: "party_name",
+			label: __("Party Name"),
+			fieldtype: "Data",
+			hidden: 1,
+		},
+		{
+			fieldname: "categorize_by",
+			label: __("Categorize by"),
+			fieldtype: "Select",
+			options: [
+				"",
+				{
+					label: __("Categorize by Voucher"),
+					value: "Categorize by Voucher",
+				},
+				{
+					label: __("Categorize by Voucher (Consolidated)"),
+					value: "Categorize by Voucher (Consolidated)",
+				},
+				{
+					label: __("Categorize by Account"),
+					value: "Categorize by Account",
+				},
+				{
+					label: __("Categorize by Party"),
+					value: "Categorize by Party",
+				},
+			],
+			default: "Categorize by Voucher (Consolidated)",
+		},
+		{
+			fieldname: "tax_id",
+			label: __("Tax Id"),
+			fieldtype: "Data",
+			hidden: 1,
+		},
+		{
+			fieldname: "presentation_currency",
+			label: __("Currency"),
+			fieldtype: "Select",
+			options: erpnext.get_presentation_currency_list(),
+		},
+		{
+			fieldname: "cost_center",
+			label: __("Cost Center"),
+			fieldtype: "MultiSelectList",
+			options: "Cost Center",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Cost Center", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+		},
+		{
+			fieldname: "project",
+			label: __("Project"),
+			fieldtype: "MultiSelectList",
+			options: "Project",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Project", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+		},
+		{
+			fieldname: "include_dimensions",
+			label: __("Consider Accounting Dimensions"),
+			fieldtype: "Check",
+			default: 1,
+		},
+		{
+			fieldname: "show_opening_entries",
+			label: __("Show Opening Entries"),
+			fieldtype: "Check",
+		},
+		{
+			fieldname: "include_default_book_entries",
+			label: __("Include Default FB Entries"),
+			fieldtype: "Check",
+			default: 1,
+		},
+		{
+			fieldname: "show_cancelled_entries",
+			label: __("Show Cancelled Entries"),
+			fieldtype: "Check",
+		},
+		{
+			fieldname: "show_net_values_in_party_account",
+			label: __("Show Net Values in Party Account"),
+			fieldtype: "Check",
+		},
+		{
+			fieldname: "show_remarks",
+			label: __("Show Remarks"),
+			fieldtype: "Check",
+		},
+		{
+			fieldname: "ignore_err",
+			label: __("Ignore Exchange Rate Revaluation Journals"),
+			fieldtype: "Check",
+		},
+		{
+			fieldname: "ignore_cr_dr_notes",
+			label: __("Ignore System Generated Credit / Debit Notes"),
+			fieldtype: "Check",
+		},
+		{
+			fieldname: "territory",
+			label: __("Territory"),
+			fieldtype: "Data",
+			hidden: 1
+		},
+		{
+			fieldname: "customer_group",
+			label: __("Customer Group"),
+			fieldtype: "Data",
+			hidden: 1
+		},
+		{
+			fieldname: "custom_student_level",
+			label: __("Custom Student level"),
+			fieldtype: "Data",
+			hidden: 1
+		},
+		{
+			fieldname: "custom_student_phone_no_1",
+			label: __("Custom Student Phone No 1"),
+			fieldtype: "Data",
+			hidden: 1
+		},
+	],
+};
+
+erpnext.utils.add_dimensions("General Ledger", 15);
+
+function get_customer_details(){
+	let party_type = frappe.query_report.get_filter_value("party_type");
+	let party = frappe.query_report.get_filter_value("party");
+
+	frappe.db.get_value('Customer', party[0], ['territory', 'customer_group', 'custom_student_level', 'custom_student_phone_no_1'])
+	.then(r => {
+	let values = r.message;
+	frappe.query_report.set_filter_value("territory", values.territory);
+	frappe.query_report.set_filter_value("customer_group", values.customer_group);
+	frappe.query_report.set_filter_value("custom_student_level", values.customer_group);
+	frappe.query_report.set_filter_value("custom_student_phone_no_1", values.customer_group);
+	})
+	console.log(party_type, party)
+}
